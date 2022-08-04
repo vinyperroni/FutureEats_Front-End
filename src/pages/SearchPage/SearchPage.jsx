31e6dbb912a61f98223ab480de7548a2e33d2dae
Arlindo Vinicius Amado Nascimento Silva <viniciusamado7@gmail.com>
2022-08-04 16:26:39 -0300

    Dando inicio a pagina de Busca

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,5 +1,27 @@
+import { TextField , InputAdornment } from "@mui/material"
+import React from "react"
+import { SearchPageContainer } from "./styled"
+import lupa from "../../assets/search.png"
+import {Header} from "../../components/Header/Header"
+
 export default function SearchPage() {
-    return <div>
-        <h1>Busca</h1>
-    </div>
+    return (
+        <>
+        <Header/>
+        <SearchPageContainer>
+            <TextField
+            id="input-with-icon-textfield"
+            placeholder="Busca"
+            InputProps={{
+            startAdornment: (
+                <InputAdornment position="start">
+                    <img src={lupa} alt="lupa" />
+                </InputAdornment>
+            ),
+            }}
+        />
+            
+        </SearchPageContainer>
+        </>
+   )
 }
