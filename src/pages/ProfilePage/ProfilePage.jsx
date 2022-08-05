f848ef1222772b37db6aa97941135dbbc3ea2071
Pablo Leão <pablo_leao@hotmail.com>
2022-08-05 11:27:36 -0300

    Correção Profile Page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -21,7 +21,7 @@ export default function ProfilePage() {
             .get(OrdersHistoryGET, {
                 headers: {
                     "Content-Type": "application/json",
-                    auth: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlbU1QdmlyeTB2UmhZWXUxVU1JIiwibmFtZSI6IkxpbGFUZXN0IiwiZW1haWwiOiJsaWxhdGVzdEBsaWxhdGVzdC5jb20iLCJjcGYiOiI2NjYuOTk5LjY2Ni05NiIsImhhc0FkZHJlc3MiOnRydWUsImFkZHJlc3MiOiJSLiBBZm9uc28gQnJheiwgMTc3LCA3MSAtIFZpbGEgTi4gQ29uY2Vpw6fDo28iLCJpYXQiOjE2NTk1MzQ0NzF9.inc_tQdZqdL8Q-lj_G40pf_RKCIdy0AYspgaBJpo348"
+                    auth: window.localStorage.getItem("tknFutureEats")
                 }
             })
             .then((res) => {
